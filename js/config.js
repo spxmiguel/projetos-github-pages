8565a3bcf6d90dc4c32c86ebfe5f3559af71e6f4
Miguel <miguel.r.moretti.00@gmail.com>
2026-06-04 14:40:54 -0300

    Update site configuration via Admin Panel

--- js/config.js
+++ js/config.js
@@ -2,14 +2,12 @@ window.PORTFOLIO_CONFIG = {
   githubUsername: "spxmiguel",
   repoName: "projetos-github-pages",
   siteTitle: "Projetos GitHub",
-  siteDescription:
-    "Uma vitrine estática que sincroniza automaticamente com repositórios, GitHub Pages, README e releases oficiais.",
+  siteDescription: "Uma vitrine estática que sincroniza automaticamente com repositórios, GitHub Pages, README e releases oficiais.",
   profileImageUrl: "",
   forkUrl: "https://github.com/spxmiguel/projetos-github-pages/releases/latest/download/projetos-github-pages-universal.zip",
   ctaMode: "top",
   includeForks: false,
   includeArchived: true,
   sortBy: "updated",
-  encryptedToken: "",
+  encryptedToken: "2380952270cb6a665d45ed63936e3660.9025d9fa508a992600cff7a7.da54a874eab5511d08c9e53976b18d811ef49af80470bc4dde2a9f0ab6f9b150d3569432c346a11876432f446c06b3500ebcaffb9412bc2c",
 };
-
